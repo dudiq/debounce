93072289a11e8a9efaf1fcc32e57e216a3c7a3a7
dudiq <idudiq@gmail.com>
2018-12-06 09:08:13 +0300

    compiled lib to es5 standart

--- debounce.js
+++ debounce.js
@@ -1,8 +1,16 @@
-const DEFAULT_TIMEOUT = 200;
+"use strict";
 
-export default function debounce(timeout = DEFAULT_TIMEOUT) {
-    const map = {};
-    const func = function (id, cb, params) {
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = debounce;
+var DEFAULT_TIMEOUT = 200;
+
+function debounce() {
+    var timeout = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : DEFAULT_TIMEOUT;
+
+    var map = {};
+    var func = function func(id, cb, params) {
         if (typeof id == "function") {
             params = cb;
             cb = id;
